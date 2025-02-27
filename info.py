f18907b5f69b2bf9fb231833411db493948b0263
Anujith18 <anujithk88@gmail.com>
2025-02-28 02:12:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 
 # MongoDB information
-
+SECONDDB_URI = environ.get('SECONDDB_URI', None)
 DATABASE_URI = environ.get(DATABASE_URI, "mongodb+srv://GG:GG@ggg.in1al.mongodb.net/NIKHIL?retryWrites=true&w=majority&appName=GGG")
 DATABASE_NAME = environ.get('DATABASE_NAME', "GG")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'WWW')
