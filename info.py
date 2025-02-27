3416308513489034a75234e9faa9e78ec3dbdb7c
Anujith18 <anujithk88@gmail.com>
2025-02-28 02:21:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 
 # MongoDB information
 SECONDDB_URI = environ.get('SECONDDB_URI', None)
-DATABASE_URI = environ.get(DATABASE_URI, "mongodb+srv://GG:GG@ggg.in1al.mongodb.net/NIKHIL?retryWrites=true&w=majority&appName=GGG")
+DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "GG")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'WWW')
 
